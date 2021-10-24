c50103930c13080153ad1c5e8d4c1644bab654a9
Milagros Pernia Gonzalez <starrymiracles@gmail.com>
2021-10-24 19:41:46 -0400

    Added jquery and smooth scroll behavior

--- javascript/jsc.js
+++ javascript/jsc.js
@@ -1,25 +1,24 @@
-// Get the modal
-let modal = document.getElementById("myModal");
+jQuery(($) => {
+  // The speed of the scroll in milliseconds
+  const speed = 1000;
 
-// Get the button that opens the modal
-let btn = document.getElementById("myBtn");
+  $('a[href*="#"]')
+    .filter(
+      (i, a) =>
+        a.getAttribute("href").startsWith("#") ||
+        a.href.startsWith(`${location.href}#`)
+    )
+    .unbind("click.smoothScroll")
+    .bind("click.smoothScroll", (event) => {
+      const targetId = event.currentTarget.getAttribute("href").split("#")[1];
+      const targetElement = document.getElementById(targetId);
 
-// Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function () {
-  modal.style.display = "block";
-};
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
+      if (targetElement) {
+        event.preventDefault();
+        $("html, body").animate(
+          { scrollTop: $(targetElement).offset().top },
+          speed
+        );
+      }
+    });
+});
